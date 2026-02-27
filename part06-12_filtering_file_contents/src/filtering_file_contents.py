deca7f151149113a2b71013054b48d027226baf8
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-02-27 19:10:07 +0100

    solution

--- part06-12_filtering_file_contents/src/filtering_file_contents.py
+++ part06-12_filtering_file_contents/src/filtering_file_contents.py
@@ -1,32 +1,51 @@
-# Write your solution here
 def filter_solutions():
-    correct = []
-    incorrect = []
+    with open("solutions.csv") as source, open("correct.csv", "w") as correct_file, open("incorrect.csv", "w") as incorrect_file:
+        for row in source:
+            pieces = row.split(";")
 
-    with open("solutions.csv") as file:
-        for line in file:
-            line = line.strip()
-            parts = line.split(";")
+            calculation = pieces[1]
+            result = pieces[2]
 
-            name = parts[0]
-            problem = parts[1]
-            given_answer = int(parts[2])
-
-            if "+" in problem:
-                operands = problem.split("+")
-                correct_answer = int(operands[0]) + int(operands[1])
+            if "+" in calculation:
+                operands = calculation.split("+")
+                correct = int(operands[0]) + int(operands[1]) == int(result)
             else:
-                operands = problem.split("-")
-                correct_answer = int(operands[0]) - int(operands[1])
+                operands = calculation.split("-")
+                correct = int(operands[0]) - int(operands[1]) == int(result)
+
+            if correct:
+                correct_file.write(row)
+            else:
+                incorrect_file.write(row)
+# # Write your solution here
+# def filter_solutions():
+#     correct = []
+#     incorrect = []
+
+#     with open("solutions.csv") as file:
+#         for line in file:
+#             line = line.strip()
+#             parts = line.split(";")
+
+#             name = parts[0]
+#             problem = parts[1]
+#             given_answer = int(parts[2])
+
+#             if "+" in problem:
+#                 operands = problem.split("+")
+#                 correct_answer = int(operands[0]) + int(operands[1])
+#             else:
+#                 operands = problem.split("-")
+#                 correct_answer = int(operands[0]) - int(operands[1])
             
-            if given_answer == correct_answer:
-                correct.append(line)
-            else:
-                incorrect.append(line)
+#             if given_answer == correct_answer:
+#                 correct.append(line)
+#             else:
+#                 incorrect.append(line)
 
-    with open("correct.csv", "w") as correct_file:
-        for line in correct:
-            correct_file.write(line + "\n")
-    with open("incorrect.csv", "w") as incorrect_file:
-        for line in incorrect:
-            incorrect_file.write(line + "\n")        
+#     with open("correct.csv", "w") as correct_file:
+#         for line in correct:
+#             correct_file.write(line + "\n")
+#     with open("incorrect.csv", "w") as incorrect_file:
+#         for line in incorrect:
+#             incorrect_file.write(line + "\n")        
