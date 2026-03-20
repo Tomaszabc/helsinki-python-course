0668de573d8d0edff9b83e9bccef1323450f609c
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-03-20 07:53:00 +0100

    read_input

--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def read_input(prompt, lower_bound, upper_bound):
+    while True:
+        try:
+            number = int(input(prompt))
+            if lower_bound <= number <= upper_bound:
+                return number
+            else:
+                 print(f"You must type in an integer between {lower_bound} and {upper_bound}")
+        except ValueError:
+            print(f"You must type in an integer between {lower_bound} and {upper_bound}")
+  
+
+# number = read_input("Please type in a number: ", 5, 10)
+# print("You typed in:", number)
