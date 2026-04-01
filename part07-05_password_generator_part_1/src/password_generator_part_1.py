c5ee2dd4c2ce45eb2fb390d761fd74ad0f53fc94
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-04-01 13:19:04 +0200

    password radomize

--- part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,13 @@
 # Write your solution here
+from random import randint
+import string
+
+def generate_password(length: int):
+    password = []
+    while len(password) < length:
+        random_index = randint(0,len(string.ascii_lowercase) - 1)
+        password.append(string.ascii_lowercase[random_index])
+    return "".join(password)
+
+# for i in range(10):
+#     print(generate_password(8))
