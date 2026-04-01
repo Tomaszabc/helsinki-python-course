b3e2d2f7483907c5347a8e17369b8bac75750d3f
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-04-01 13:48:06 +0200

    strong password

--- part07-06_password_generator_part_2/src/password_generator_part_2.py
+++ part07-06_password_generator_part_2/src/password_generator_part_2.py
@@ -1,1 +1,31 @@
 # Write your solution here
+# Write your solution here
+from random import choice, randint, shuffle
+from string import ascii_lowercase, digits
+
+def generate_strong_password(length: int, include_numbers: bool, include_special: bool):
+    lowercase = ascii_lowercase
+    numbers = digits
+    special = "!?=+-()#"   
+
+    characters = lowercase
+    if include_numbers:
+        characters += numbers
+    if include_special:
+        characters += special
+
+    password = []
+
+    password.append(choice(lowercase))
+
+    if include_numbers:
+        password.append(choice(numbers))
+    if include_special:
+        password.append(choice(special))
+
+    while len(password) < length:
+        password.append(choice(characters))
+
+        shuffle(password)
+    
+    return "".join(password)
