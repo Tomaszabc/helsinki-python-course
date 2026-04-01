f208462e520422ebe58f077c4116291d4c2ab952
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-04-01 13:23:31 +0200

    solution

--- part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,13 +1,23 @@
 # Write your solution here
-from random import randint
-import string
+from random import choice
+from string import ascii_lowercase
 
 def generate_password(length: int):
-    password = []
-    while len(password) < length:
-        random_index = randint(0,len(string.ascii_lowercase) - 1)
-        password.append(string.ascii_lowercase[random_index])
-    return "".join(password)
+    passwd = ""
+    for i in range(length):
+        passwd += choice(ascii_lowercase)
+    
+    return passwd
+
+# from random import randint
+# import string
+
+# def generate_password(length: int):
+#     password = []
+#     while len(password) < length:
+#         random_index = randint(0,len(string.ascii_lowercase) - 1)
+#         password.append(string.ascii_lowercase[random_index])
+#     return "".join(password)
 
 # for i in range(10):
 #     print(generate_password(8))
