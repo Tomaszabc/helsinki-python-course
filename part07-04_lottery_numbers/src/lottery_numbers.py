937104991a7e11c9396a771365b8b22091a717e7
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-04-01 12:29:08 +0200

    random numbers

--- part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,17 @@
 # Write your solution here
+from random import randint
+
+def lottery_numbers(amount: int, lower: int, upper:int):
+    random_list = []
+    while True:
+        random_number = randint(lower, upper)
+        if random_number not in random_list:
+            random_list.append(random_number)
+
+        if  len(random_list) == amount:
+            break
+    random_list.sort()
+    return random_list
+
+# for number in lottery_numbers(7, 1, 40):
+#     print(number)
