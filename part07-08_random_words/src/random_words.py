3f0c436e19154bc622cc87143b173e6c3a5d65a1
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-04-03 13:36:45 +0200

    random words

--- part07-08_random_words/src/random_words.py
+++ part07-08_random_words/src/random_words.py
@@ -1,1 +1,14 @@
 # Write your solution here
+import random
+
+def words(n: int, beginning: str):
+    word_list = []
+
+    with open("words.txt") as file:
+        for line in file:
+            word = line.strip()
+            if word.startswith(beginning):
+                word_list.append(word)
+    if len(word_list) < n:
+        raise ValueError(f"Not enough words beginning with '{beginning}'")
+    return random.sample(word_list, n)
