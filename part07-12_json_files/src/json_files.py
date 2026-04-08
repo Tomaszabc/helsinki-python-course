57b49ef8eac3477841499250fb77493dc335acf6
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-04-08 08:47:42 +0200

    students

--- part07-12_json_files/src/json_files.py
+++ part07-12_json_files/src/json_files.py
@@ -1,1 +1,15 @@
 # Write your solution here
+import json
+
+def print_persons(filename: str):
+    with open(filename) as my_file:
+        data = my_file.read()
+        students = json.loads(data)
+
+        for student in students:
+            name = student["name"]
+            age = student["age"]
+            hobbies = student["hobbies"]
+
+            hobbies_str = ", ".join(hobbies)
+            print(f"{name} {age} years ({hobbies_str})")
