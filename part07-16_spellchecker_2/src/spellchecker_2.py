4786902ed80611a5ffb2a2fd86ab1665a24c46c2
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-04-17 12:50:07 +0200

    spellchecker 2

--- part07-16_spellchecker_2/src/spellchecker_2.py
+++ part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,56 @@
-# Write your solution here
+# write your solution here
+from difflib import get_close_matches
+
+if True:
+    user_text = input("Write text: ")
+else:
+    user_text = "We use ptython to make a spell checker"
+
+wordlist = "wordlist.txt"
+
+# Wczytaj poprawne słowa do listy
+correct_words = []
+with open(wordlist) as file:
+    for line in file:
+        word = line.strip()
+        correct_words.append(word.lower())
+
+# Podziel tekst na słowa
+words = user_text.split()
+
+# Znajdź błędne słowa i zaproponuj sugestie
+final_parts = []
+misspelled = {}  # słownik: błędne słowo -> lista sugestii
+
+for word in words:
+    # Usuń znaki interpunkcyjne dla sprawdzenia
+    clean_word = word.strip(".,!?;:()\"'")
+    
+    if clean_word.lower() in correct_words:
+        final_parts.append(word)  # poprawne słowo - bez gwiazdek
+    else:
+        final_parts.append(f"*{word}*")  # błędne słowo - z gwiazdkami
+        
+        # Znajdź sugestie tylko dla oryginalnego słowa (bez interpunkcji)
+        if clean_word.lower() not in misspelled:
+            suggestions = get_close_matches(clean_word.lower(), correct_words, n=3, cutoff=0.6)
+            if suggestions:
+                misspelled[clean_word.lower()] = suggestions
+
+# Wydrukuj poprawiony tekst
+print(" ".join(final_parts))
+
+# Wydrukuj sugestie
+if misspelled:
+    print("suggestions:")
+    for wrong_word, suggestions in misspelled.items():
+        # Znajdź oryginalne słowo z interpunkcją (dla wyświetlenia)
+        original_word = None
+        for word in words:
+            if word.strip(".,!?;:()\"'").lower() == wrong_word:
+                original_word = word
+                break
+        
+        # Wyświetl sugestie
+        suggestion_str = ", ".join(suggestions)
+        print(f"{original_word}: {suggestion_str}")
