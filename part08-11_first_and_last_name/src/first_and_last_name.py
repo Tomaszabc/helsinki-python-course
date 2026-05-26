c67d34d2ea6c3e6186ad26e2a6355bb41b8e3840
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-05-26 14:17:10 +0200

    first and last name

--- part08-11_first_and_last_name/src/first_and_last_name.py
+++ part08-11_first_and_last_name/src/first_and_last_name.py
@@ -1,4 +1,24 @@
 # Write your solution here:
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+
+    def return_first_name(self):
+        splitted_name = self.name.split(" ")
+        return splitted_name[0]
+
+    def return_last_name(self):
+        splitted_name = self.name.split(" ")
+        return splitted_name[-1]
+
+if __name__ == "__main__":
+    peter = Person("Peter Pythons")
+    print(peter.return_first_name())
+    print(peter.return_last_name())
+
+    paula = Person("Paula Pythonnen")
+    print(paula.return_first_name())
+    print(paula.return_last_name())
 
 
 
