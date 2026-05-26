a7131f146be47c13ea4e00acf156367ba170861e
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-05-26 14:50:45 +0200

    number_stats part 1

--- part08-12_number_stats/src/number_stats.py
+++ part08-12_number_stats/src/number_stats.py
@@ -2,9 +2,19 @@
 class  NumberStats:
     def __init__(self):
         self.numbers = 0
+        self.count = 0
+        self.sum = 0
 
     def add_number(self, number:int):
-        pass
+        self.count += 1
+        self.sum += number
 
     def count_numbers(self):
-        pass
+        return self.count
+
+stats = NumberStats()
+stats.add_number(3)
+stats.add_number(5)
+stats.add_number(1)
+stats.add_number(2)
+print("Numbers added:", stats.count_numbers())
