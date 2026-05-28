147709b4dc7be03557d4cf5f2bba9bf2158f5c5d
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-05-28 14:56:27 +0200

    series'

--- part08-16_series/src/series.py
+++ part08-16_series/src/series.py
@@ -1,1 +1,13 @@
-# Write your solution here:
+class Series:
+    def __init__(self, title: str, seasons: int, genres: list):
+        self.title = title
+        self.seasons = seasons  
+        self.genres = genres
+
+    def __str__(self):
+        genre_list = self.genres
+        genre_string = ", ".join(genre_list)
+        return f"{self.title} ({self.seasons} seasons) \ngenres: {genre_string} \nno ratings"
+
+dexter = Series("Dexter", 8, ["Crime", "Drama", "Mystery", "Thriller"])
+print(dexter)
