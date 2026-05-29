e331aff92cf51ca2d08fd2a9ca54cf20cbcca928
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-05-29 14:43:56 +0200

    rate part 1

--- part08-16_series/src/series.py
+++ part08-16_series/src/series.py
@@ -3,11 +3,26 @@ class Series:
         self.title = title
         self.seasons = seasons  
         self.genres = genres
+        self.ratings = []
+
+    def rate(self, rating:int):
+        self.ratings.append(rating)
 
     def __str__(self):
-        genre_list = self.genres
-        genre_string = ", ".join(genre_list)
-        return f"{self.title} ({self.seasons} seasons) \ngenres: {genre_string} \nno ratings"
+        genre_string = ", ".join(self.genres)
+
+        if len(self.ratings) == 0:
+            return f"{self.title} ({self.seasons} seasons) \ngenres: {genre_string} \nno ratings"
+        else:
+            number = len(self.ratings)
+            average = sum(self.ratings) / number
+            return f"{self.title} ({self.seasons} seasons) \ngenres: {genre_string} \n{number} ratings, average {average:.1f} points"
+
 
 dexter = Series("Dexter", 8, ["Crime", "Drama", "Mystery", "Thriller"])
+dexter.rate(4)
+dexter.rate(5)
+dexter.rate(5)
+dexter.rate(3)
+dexter.rate(0)
 print(dexter)
