caf18ca696f4211102454ba2eed172bf9fa77d4f
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-06-02 08:54:02 +0200

    series

--- part08-16_series/src/series.py
+++ part08-16_series/src/series.py
@@ -7,6 +7,7 @@ class Series:
 
     def rate(self, rating:int):
         self.ratings.append(rating)
+    
 
     def __str__(self):
         genre_string = ", ".join(self.genres)
@@ -18,11 +19,39 @@ class Series:
             average = sum(self.ratings) / number
             return f"{self.title} ({self.seasons} seasons) \ngenres: {genre_string} \n{number} ratings, average {average:.1f} points"
 
+def minimum_grade(rating: float, series_list: list):
+    result = []
+    for series in series_list:
+        if len(series.ratings) > 0:
+            avg = sum(series.ratings) / len(series.ratings)
+            if avg >= rating:
+                result.append(series)
+    return result
 
-dexter = Series("Dexter", 8, ["Crime", "Drama", "Mystery", "Thriller"])
-dexter.rate(4)
-dexter.rate(5)
-dexter.rate(5)
-dexter.rate(3)
-dexter.rate(0)
-print(dexter)
+def includes_genre(genre: str, series_list: list):
+    result = []
+    for series in series_list:
+        if genre in series.genres:
+            result.append(series)
+    return result
+
+
+
+# s1 = Series("Dexter", 8, ["Crime", "Drama", "Mystery", "Thriller"])
+# s1.rate(5)
+
+# s2 = Series("South Park", 24, ["Animation", "Comedy"])
+# s2.rate(3)
+
+# s3 = Series("Friends", 10, ["Romance", "Comedy"])
+# s3.rate(2)
+
+# series_list = [s1, s2, s3]
+
+# print("a minimum grade of 4.5:")
+# for series in minimum_grade(4.5, series_list):
+#     print(series.title)
+
+# print("genre Comedy:")
+# for series in includes_genre("Comedy", series_list):
+#     print(series.title)
