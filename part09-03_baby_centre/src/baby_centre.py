04d98082fc6df66b629d20a9296cdbaa8853b8c9
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-06-11 14:46:57 +0200

    Weigh in

--- part09-03_baby_centre/src/baby_centre.py
+++ part09-03_baby_centre/src/baby_centre.py
@@ -14,28 +14,33 @@ class BabyCentre:
         self.number_of_weigh_ins = 0
 
     def weigh(self, person: Person):
+        self.number_of_weigh_ins += 1
         # return the weight of the person passed as an argument
         return person.weight
 
     def feed(self, person: Person):
         person.weight += 1
+    
+    def weigh_ins(self):
+        return self.number_of_weigh_ins
 
+if __name__ == "__main__":
 
+    baby_centre = BabyCentre()
 
+    eric = Person("Eric", 1, 110, 7)
+    peter = Person("Peter", 33, 176, 85)
 
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
 
-baby_centre = BabyCentre()
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
 
-eric = Person("Eric", 1, 110, 7)
-peter = Person("Peter", 33, 176, 85)
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
 
-print(f"{eric.name} weighs {baby_centre.weigh(eric)} kg")
-print(f"{peter.name} weighs {baby_centre.weigh(peter)} kg")
-print() 
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
 
-baby_centre.feed(eric)
-baby_centre.feed(eric)
-baby_centre.feed(eric)
-
-print(f"{eric.name} weighs {baby_centre.weigh(eric)} kg")
-print(f"{peter.name} weighs {baby_centre.weigh(peter)} kg")
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
