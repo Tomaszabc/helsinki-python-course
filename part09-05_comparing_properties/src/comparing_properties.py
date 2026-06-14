32a461a5a8cf77039bf422672d62081ae710f1ea
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-06-14 19:29:36 +0200

    price_diff

--- part09-05_comparing_properties/src/comparing_properties.py
+++ part09-05_comparing_properties/src/comparing_properties.py
@@ -11,10 +11,15 @@ class RealProperty:
             return True
         else: 
             return False
+        
+    def price_difference(self, compared_to: "RealProperty"):
+        self_price = self.square_metres * self.price_per_sqm
+        compared_to_price = compared_to.square_metres * compared_to.price_per_sqm
+        return abs(self_price - compared_to_price)
 
 central_studio = RealProperty(1, 16, 5500)
 downtown_two_bedroom = RealProperty(2, 38, 4200)
 suburbs_three_bedroom = RealProperty(3, 78, 2500)
 
-print(central_studio.bigger(downtown_two_bedroom))
-print(suburbs_three_bedroom.bigger(downtown_two_bedroom))
+print(central_studio.price_difference(downtown_two_bedroom))
+print(suburbs_three_bedroom.price_difference(downtown_two_bedroom))
