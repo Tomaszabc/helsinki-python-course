231fd5fe6e69a4dffa7601b246e441d081d5f245
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-06-16 14:58:18 +0200

    shortest

---
+++ part09-08_shortest_in_room/src/shortest_in_room.py
@@ -0,0 +1,41 @@
+# WRITE YOUR SOLUTION HERE:
+class Person:
+    def __init__(self, name: str, height: int):
+        self.name = name
+        self.height = height
+
+    def __str__(self):
+        return self.name
+
+class Room:
+    def __init__(self):
+        self.persons = []
+
+    def add(self, person: Person):
+        self.persons.append(person)
+    
+    def is_empty(self):
+        return len(self.persons) == 0
+
+    def print_contents(self):
+        if not self.is_empty = False:
+            combined_height = 0
+            for person in self.persons:
+                combined_height += person.height
+            print(f"There are {len(self.persons)} persons in the room, and their combined height is {combined_height} cm")
+            for person in self.persons:
+                print(person)
+
+
+
+
+
+room = Room()
+print("Is the room empty?", room.is_empty())
+room.add(Person("Lea", 183))
+room.add(Person("Kenya", 172))
+room.add(Person("Ally", 166))
+room.add(Person("Nina", 162))
+room.add(Person("Dorothy", 155))
+print("Is the room empty?", room.is_empty())
+room.print_contents()
