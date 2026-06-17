7c10db13681605902710ab509661ecce2e7bb60e
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-06-17 11:37:16 +0200

    shortest

--- part09-08_shortest_in_room/src/shortest_in_room.py
+++ part09-08_shortest_in_room/src/shortest_in_room.py
@@ -18,7 +18,7 @@ class Room:
         return len(self.persons) == 0
 
     def print_contents(self):
-        if not self.is_empty:
+        if not self.is_empty():
             combined_height = 0
             for person in self.persons:
                 combined_height += person.height
@@ -26,16 +26,50 @@ class Room:
             for person in self.persons:
                 print(f"{person.name} ({person.height} cm)")
 
+    def shortest(self):
+        if self.is_empty():
+            return None
+
+        shortest = self.persons[0]
+        for person in self.persons:
+            if person.height < shortest.height:
+                shortest = person
+        return shortest
+    
+    def remove_shortest(self):
+        if self.is_empty():
+            return None
+        
+        shortest = self.persons[0]
+        for person in self.persons:
+            if person.height < shortest.height:
+                shortest = person
+        
+        self.persons.remove(shortest)
+
+        return shortest
+        
 
 
 
 
-room = Room()
-print("Is the room empty?", room.is_empty())
-room.add(Person("Lea", 183))
-room.add(Person("Kenya", 172))
-room.add(Person("Ally", 166))
-room.add(Person("Nina", 162))
-room.add(Person("Dorothy", 155))
-print("Is the room empty?", room.is_empty())
-room.print_contents()
+
+
+
+if __name__ == "__main__":
+    room = Room()
+
+    room.add(Person("Lea", 183))
+    room.add(Person("Kenya", 172))
+    room.add(Person("Nina", 162))
+    room.add(Person("Ally", 166))
+    room.print_contents()
+
+    print()
+
+    removed = room.remove_shortest()
+    print(f"Removed from room: {removed.name}")
+
+    print()
+
+    room.print_contents()
