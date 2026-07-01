4dab3afde06cbf7bc46962d78c1461534c986354
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-07-01 20:09:46 +0200

    list helper

---
+++ part09-14_list_helper/src/list_helper.py
@@ -0,0 +1,41 @@
+# WRITE YOUR SOLUTION HERE:
+class ListHelper:
+    @classmethod
+    def greatest_frequency(cls, my_list: list):
+        counts = {}
+        for item in my_list:
+            if item in counts:
+                counts[item] = counts[item] + 1
+            else:
+                counts[item] = 1
+
+        most_common = None
+        max_count = 0
+        for item, count in counts.items():
+            if count > max_count:
+                max_count = count
+                most_common = item
+
+        return most_common
+
+    @classmethod
+    def doubles(cls, my_list: list):
+        counts = {}
+        for item in my_list:
+            if item in counts:
+                counts[item] = counts[item] + 1
+            else:
+                counts[item] = 1
+
+        double_count = 0
+        for count in counts.values():
+            if count >= 2:
+                double_count += 1
+
+        return double_count
+
+    
+
+numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
+print(ListHelper.greatest_frequency(numbers))
+print(ListHelper.doubles(numbers))
