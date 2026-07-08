f19bcda4133847beb982f0def24a9ce26a7f9130
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-07-08 14:50:32 +0200

    comparators

--- part10-07_money/src/money.py
+++ part10-07_money/src/money.py
@@ -7,10 +7,32 @@ class Money:
     def __str__(self):
         return f"{self.euros}.{self.cents:02d} eur"
 
+    def __eq__(self, another):
+        return (self.euros + (self.cents/100)) == (another.euros + (another.cents/100))
 
+    def __ne__(self, another):
+        return (self.euros + (self.cents/100)) != (another.euros + (another.cents/100))
 
-e1 = Money(4, 10)
-e2 = Money(2, 5)  # two euros and five cents
+    def __lt__(self, another):
+        return (self.euros + (self.cents/100)) < (another.euros + (another.cents/100))
+    def __gt__(self, another):
+        return (self.euros + (self.cents/100)) > (another.euros + (another.cents/100))
 
-print(e1)
-print(e2)
+if __name__ == "__main__":
+    e1 = Money(4, 10)
+    e2 = Money(2, 5)
+    e3 = Money(4, 10)
+
+    print(e1)
+    print(e2)
+    print(e3)
+    print(e1 == e2)
+    print(e1 == e3)
+
+    e1 = Money(4, 10)
+    e2 = Money(2, 5)
+
+    print(e1 != e2)
+    print(e1 < e2)
+    print(e1 > e2)
+    
