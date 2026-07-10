018cc23b15797f752804f7ec9c2a4dbd013a2e8c
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-07-10 14:58:34 +0200

    money pt 3

--- part10-07_money/src/money.py
+++ part10-07_money/src/money.py
@@ -7,32 +7,37 @@ class Money:
     def __str__(self):
         return f"{self.euros}.{self.cents:02d} eur"
 
+    def __value(self):
+        return self.euros * 100 + self.cents
+
     def __eq__(self, another):
-        return (self.euros + (self.cents/100)) == (another.euros + (another.cents/100))
+        return self.__value() == another.__value()
 
     def __ne__(self, another):
-        return (self.euros + (self.cents/100)) != (another.euros + (another.cents/100))
+        return self.__value() != another.__value()
 
     def __lt__(self, another):
-        return (self.euros + (self.cents/100)) < (another.euros + (another.cents/100))
+        return self.__value() < another.__value()
+
     def __gt__(self, another):
-        return (self.euros + (self.cents/100)) > (another.euros + (another.cents/100))
+        return self.__value() > another.__value()
+
+    def __add__(self, another):
+        return self.__value() + another.__value()
+
+    def __sub__(self, another):
+        return self.__value() - another.__value()
+
 
 if __name__ == "__main__":
-    e1 = Money(4, 10)
-    e2 = Money(2, 5)
-    e3 = Money(4, 10)
+    e1 = Money(4, 5)
+    e2 = Money(2, 95)
 
-    print(e1)
-    print(e2)
+    e3 = e1 + e2
+    e4 = e1 - e2
+
     print(e3)
-    print(e1 == e2)
-    print(e1 == e3)
+    print(e4)
 
-    e1 = Money(4, 10)
-    e2 = Money(2, 5)
-
-    print(e1 != e2)
-    print(e1 < e2)
-    print(e1 > e2)
+    e5 = e2-e1
     
