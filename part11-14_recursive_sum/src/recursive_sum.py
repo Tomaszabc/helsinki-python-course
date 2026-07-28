76783ad8005ccf012239546da52a4a4a87051996
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-07-28 14:53:19 +0200

    recursive sum not finish

--- part11-14_recursive_sum/src/recursive_sum.py
+++ part11-14_recursive_sum/src/recursive_sum.py
@@ -1,1 +1,19 @@
 # WRITE YOUR SOLUTION HERE:
+def recursive_sum(number: int):
+    if number <= 1:
+        return number
+    sum = 0
+    sum += number
+    number -= 1
+    if number == 1:
+        return sum + 1
+    else:
+        recursive_sum(number)
+
+
+
+result = recursive_sum(3)
+print(result)
+
+print(recursive_sum(5))
+print(recursive_sum(10))
