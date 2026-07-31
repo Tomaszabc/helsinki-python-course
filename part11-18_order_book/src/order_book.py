f4b31d21367aa8ac04a52c2fdf34e872512e426f
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-07-31 14:08:23 +0200

    first part

--- part11-18_order_book/src/order_book.py
+++ part11-18_order_book/src/order_book.py
@@ -1,1 +1,42 @@
 # Write your solution here:
+class Task:
+    def __init__(self, description: str, name: str, workload: int):
+        self.__description = description
+        self.__workload = workload
+        self.__name = name 
+        self.__id = 0
+        self.__is_finished = False
+
+    def id(self):
+        return self.__id
+
+    def description(self):
+        return self.__description
+        
+    def programmer(self):
+        return self.__name
+
+    def workload(self):
+        return self.__workload
+
+    def is_finished(self):
+        return self.__is_finished
+
+    def mark_finished(self):
+        self.__is_finished = True
+        return self.__is_finished
+
+    def __str__(self):
+        return (f"{self.__id}:{self.__name} ({self.__workload} hours), programmer {self.__name} {self.__is_finished}")
+
+t1 = Task("program hello world", "Eric", 3)
+print(t1.id, t1.description, t1.programmer, t1.workload)
+print(t1)
+print(t1.is_finished())
+t1.mark_finished()
+print(t1)
+print(t1.is_finished())
+t2 = Task("program webstore", "Adele", 10)
+t3 = Task("program mobile app for workload accounting", "Eric", 25)
+print(t2)
+print(t3)
