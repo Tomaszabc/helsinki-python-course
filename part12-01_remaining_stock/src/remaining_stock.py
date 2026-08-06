f4f1965e54478269a196314325e820dc98ae4b9b
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-08-06 18:18:02 +0200

    remaining stock

---
+++ part12-01_remaining_stock/src/remaining_stock.py
@@ -0,0 +1,13 @@
+# Write your solution here:
+def sort_by_remaining_stock(items: list):
+    def remaining(item):
+        return item[2]
+
+    return sorted(items, key=remaining)
+
+if __name__ == "__main__":
+
+    products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22)]
+
+    for product in sort_by_remaining_stock(products):
+        print(f"{product[0]} {product[2]} pcs")
