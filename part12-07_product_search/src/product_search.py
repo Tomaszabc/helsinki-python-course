244addfa7b3fe999c9f96c24c5ceb2572d50eb7d
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-08-10 19:57:55 +0200

    product search

--- part12-07_product_search/src/product_search.py
+++ part12-07_product_search/src/product_search.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def search(products: list, criterion: callable):
+    result = []
+    for product in products:
+        if criterion(product):
+            result.append(product)
+    return result
+
+
+def price_under_4_euros(product):
+    return product[1] < 4
+
+if __name__ == "__main__":
+    products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22), ("kale", 0.99, 1)]
+
+    for product in search(products, price_under_4_euros):
+        print(product)
