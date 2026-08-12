72db614a153647db3ca405c7a10242cf539221af
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-08-12 14:58:01 +0200

    random words

--- part12-10_random_words/src/random_words.py
+++ part12-10_random_words/src/random_words.py
@@ -1,1 +1,22 @@
 # Write your solution here:
+import random
+
+
+def word_generator(characters: str, length: int, amount: int):
+    for _ in range(amount):
+        yield ''.join(random.choice(characters) for _ in range(length))
+
+
+
+    # random_words = []
+    # while len(random_words) < amount:
+    #     random_words.append(random.choice(characters) * length)
+
+    # yield random_words
+
+
+if __name__ == "__main__":
+
+    wordgen = word_generator("abcdefg", 3, 5)
+    for word in wordgen:
+        print(word)
