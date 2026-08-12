5e36cbde2bde9a656d86ff9049c76322e0c3defc
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-08-12 13:12:57 +0200

    prime numbers

--- part12-09_prime_numbers/src/prime_numbers.py
+++ part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,21 @@
 # Write your solution here
+def prime_numbers():
+    number = 2
+    while True:
+        is_prime = True
+        for divisor in range(2, number):
+            if number % divisor == 0: 
+                is_prime = False
+                break
+        
+        if is_prime:
+            yield number
+        
+        number += 1
+
+
+if __name__ == "__main__":
+
+    numbers = prime_numbers()
+    for i in range(8):
+        print(next(numbers))
