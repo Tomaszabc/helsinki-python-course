11dcdcdb3deeccaf654bbb1684995efa6eb99c9a
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-08-17 12:58:31 +0200

    filtering 3/3

--- part12-13_credits/src/credits.py
+++ part12-13_credits/src/credits.py
@@ -19,10 +19,23 @@ class CourseAttempt:
 def sum_of_all_credits(attempts: list):
     return reduce(lambda total, attempt: total + attempt.credits, attempts, 0)
 
+def sum_of_passed_credits(attempts: list):
+    passed = filter(lambda x: x.grade > 0, attempts)
+    return reduce(lambda total, attempt: total + attempt.credits, passed, 0)
 
 
-s1 = CourseAttempt("Introduction to Programming", 5, 5)
-s2 = CourseAttempt("Advanced Course in Programming", 4, 5)
-s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
-credit_sum = sum_of_all_credits([s1, s2, s3])
-print(credit_sum)
+def average(attempts: list):
+    passed = list(filter(lambda x: x.grade > 0, attempts))
+    if not passed:
+        return 0
+    
+    total_grades = reduce(lambda total, attempt: total + attempt.grade, passed, 0)
+    return total_grades / len(passed)
+
+if __name__ == "__main__":
+
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    ag = average([s1, s2, s3])
+    print(ag)
