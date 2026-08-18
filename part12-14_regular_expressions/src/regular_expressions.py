296aa0883ad9bca21694ce88a89656ec99e04874
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-08-18 13:28:34 +0200

    regular express

--- part12-14_regular_expressions/src/regular_expressions.py
+++ part12-14_regular_expressions/src/regular_expressions.py
@@ -1,2 +1,19 @@
 # Write your solution here
 import re
+
+def is_dotw(my_string: str):
+    pattern = r"^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)$"
+    return bool(re.search(pattern, my_string))
+
+def all_vowels(my_string: str):
+    pattern = r"^[aeiouAEIOU]+$"
+    return bool(re.search(pattern, my_string))
+
+def time_of_day(my_string: str):
+    return bool(re.fullmatch(r"([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]", my_string))
+if __name__ == "__main__":
+
+    print(time_of_day("12:43:01"))
+    print(time_of_day("AB:01:CD"))
+    print(time_of_day("17:59:59"))
+    print(time_of_day("33:66:77"))
