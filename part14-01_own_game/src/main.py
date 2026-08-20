2ce9ab6705ed56e4d14a1a841755570d25f183b7
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2026-08-20 14:54:09 +0200

    part 3/4

--- part14-01_own_game/src/main.py
+++ part14-01_own_game/src/main.py
@@ -40,8 +40,46 @@ class Sokoban:
 
     def check_events(self):
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.move(0, -1)
+                if event.key == pygame.K_RIGHT:
+                    self.move(0, 1)
+                if event.key == pygame.K_UP:
+                    self.move(-1, 0)
+                if event.key == pygame.K_DOWN:
+                    self.move(1, 0)
+
             if event.type == pygame.QUIT:
                 exit()
+
+    def move(self, move_y, move_x):
+        robot_old_y, robot_old_x = self.find_robot() 
+        robot_new_y = robot_old_y + move_y
+        robot_new_x = robot_old_x + move_x
+
+        if self.map[robot_new_y][robot_new_x] == 1:
+            return
+
+        if self.map[robot_new_y][robot_new_x] in [3, 5]:
+            box_new_y = robot_new_y + move_y
+            box_new_x = robot_new_x + move_x
+
+            if self.map[box_new_y][box_new_x] in [1, 3, 5]:
+                return
+
+            self.map[robot_new_y][robot_new_x] -= 3
+            self.map[box_new_y][box_new_x] += 3
+
+        self.map[robot_old_y][robot_old_x] -= 4
+        self.map[robot_new_y][robot_new_x] += 4
+
+
+    def find_robot(self ):
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.map[y][x] in [4,6]:
+                    return (y, x)
 
     def draw_window(self):
         self.window.fill((0, 0, 0))
